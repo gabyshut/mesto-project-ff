d362dc8c380f24066f675bb9d02e17bd8839ab45
unknown <gabriella.jester@yandex.ru>
2025-01-24 16:27:48 +0300

    Первая версия проекта

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Зеленые средней высоты частично заснеженные горы. Пейзаж из Архыза.",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Река, протекающая сквозь заснеженный лес. Пейзаж из Челябинской области.",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Советские многоэтажки. Пейзаж из Иваново.",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Местность, покрытая мхом, на фоне высокой заснеженной горы. Пейзаж из Камчатки.",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Железная дорога, уходящая вдаль, посреди густого леса. Пейзаж из Холмогорского района.",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Отвесная скала возле заледенелого озера. Пейзаж из Байкала.",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const cardList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+//Функция создания карточки
 
-// @todo: Функция создания карточки
+function createCard(cardContent, deleteCard) {
+  const cardTemplateCopy = cardTemplate.cloneNode(true);
 
-// @todo: Функция удаления карточки
+  const cardImageTemplate = cardTemplateCopy.querySelector(".card__image");
+  const cardDescriptionTemplate = cardTemplateCopy.querySelector(".card__title");
+  const deleteButtonTemplate = cardTemplateCopy.querySelector(".card__delete-button");
 
-// @todo: Вывести карточки на страницу
+  cardImageTemplate.setAttribute("src", cardContent.link);
+  cardImageTemplate.setAttribute('alt', cardContent.alt);
+  cardDescriptionTemplate.textContent = cardContent.name;
+
+  deleteButtonTemplate.addEventListener("click", deleteCard);
+
+  return cardTemplateCopy;
+}
+
+//Функция удаления карточки
+
+function deleteCard(event) {
+  const listItemToDelete = event.target.closest(".places__item");
+  listItemToDelete.remove();
+}
+
+//Вывод карточки на страницу
+
+initialCards.forEach((item) => {
+  cardList.append(createCard(item, deleteCard));
+});
