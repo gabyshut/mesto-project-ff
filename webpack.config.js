5bb7c90c8fe9810bc0edd19e0342e82c139379ef
unknown <gabriella.jester@yandex.ru>
2025-12-04 16:41:23 +0300

    fix: gh pages publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -7,63 +7,63 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
     entry: { main: './src/index.js' },
+
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
-                publicPath: isProd ? '/mesto-project-ff/' : '/',
+        publicPath: isProd ? '/mesto-project-ff/' : '/',
     },
-    // mode: 'development',
-    devServer: {
-        static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-        compress: true, // это ускорит загрузку в режиме разработки
-        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-    
-        open: true // сайт будет открываться сам при запуске npm run dev
-      },
 
-      module: {
-        rules: [ // rules — это массив правил
-          // добавим в него объект правил для бабеля
-          {
-            // регулярное выражение, которое ищет все js файлы
-            test: /\.js$/,
-            // при обработке этих файлов нужно использовать babel-loader
-            use: 'babel-loader',
-            // исключает папку node_modules, файлы в ней обрабатывать не нужно
-            exclude: '/node_modules/'
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-            generator: {
-                filename: 'images/[name].[hash][ext]',
-          }
-          },
-          {
-            test: /\.(woff(2)?|eot|ttf|otf)$/i,
-            type: 'asset/resource',
-            generator: {
-                filename: 'fonts/[name].[hash][ext]',
-                }
-          },
-          {
-            // применять это правило только к CSS-файлам
-            test: /\.css$/,
-            // при обработке этих файлов нужно использовать
-            // MiniCssExtractPlugin.loader и css-loader
-            use: [MiniCssExtractPlugin.loader, {
-              loader: 'css-loader',
-              options: { importLoaders: 1 }
+    // Убираем фиксированный режим — теперь он ставится командой сборки
+    // mode: 'development',
+
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+        compress: true,
+        port: 8080,
+        open: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: '/node_modules/'
             },
-            'postcss-loader']
-          }
-          ]
-      },
-      plugins: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name].[hash][ext]',
+                }
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name].[hash][ext]',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { importLoaders: 1 }
+                    },
+                    'postcss-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
         new HtmlWebpackPlugin({
-          template: './src/index.html' // путь к файлу index.html
+            template: './src/index.html'
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin()
-      ]
+    ]
 }
